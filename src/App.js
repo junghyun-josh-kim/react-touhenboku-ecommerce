638c79599ec8bf037e8a8f5d11ab3eec767a47ff
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-10-16 22:23:55 -0400

    React: Order Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Navbar, Nav, Container, Col, Row } from 'react-bootstrap';
+import data from './data.js';
 
 function App() {
+  let [menus] = useState();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>
+            <h1>Touhenboku Eglinton</h1>
+          </Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#online-ordering' className='online-ordering'>
+              ONLINE ORDERING
+            </Nav.Link>
+            <Nav.Link href='#menu'>MENU</Nav.Link>
+            <Nav.Link href='#tomo'>TOMO</Nav.Link>
+            <Nav.Link href='#contact'>CONTACT</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'>
+        <img
+          id='logo'
+          src={process.env.PUBLIC_URL + '/img/logo.png'}
+          alt='Logo'
+        />
+      </div>
+      <Container>
+        <Row>
+          <Col xs>
+            <img
+              id='karaage'
+              src={process.env.PUBLIC_URL + '/img/karaagedon.jpg'}
+              alt='Karaage Don'
+            />
+          </Col>
+          <Col xs>Title</Col>
+          <Col xs>Description</Col>
+          <Col xs>Price</Col>
+        </Row>
+        <Row>
+          <Col xs>
+            <img
+              id='karaage'
+              src={process.env.PUBLIC_URL + '/img/karaagedon.jpg'}
+              alt='Karaage Don'
+            />
+          </Col>
+          <Col xs>Title</Col>
+          <Col xs>Description</Col>
+          <Col xs>Price</Col>
+        </Row>
+        <Row>
+          <Col xs>
+            <img
+              id='karaage'
+              src={process.env.PUBLIC_URL + '/img/karaagedon.jpg'}
+              alt='Karaage Don'
+            />
+          </Col>
+          <Col xs>Title</Col>
+          <Col xs>Description</Col>
+          <Col xs>Price</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+let data = [
+  {
+    id: 0,
+    title: 'Karaage Don',
+    decription: 'Deep fried chicken',
+    price: 14,
+  },
+  {
+    id: 1,
+    title: 'Yakitori Don',
+    description: 'Stir fired chicken',
+    price: 14,
+  },
+  {
+    id: 2,
+    title: 'Gyu Don',
+    description: 'Beef',
+    price: 16,
+  },
+];
+
+export default data;
